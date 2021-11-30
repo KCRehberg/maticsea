9e034989165911d311fc1c3c6f429ee0bc5b1027
KCRehberg <krehberg12@gmail.com>
2021-11-29 19:04:07 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,2 @@
+export const nftaddress = "0xb75b7f9775DF7d0b025e59aBB65Dd0857cd04c52"
+export const nftmarketaddress = "0xe1832d903E5d375B52aE431D3fF22A57B9a8172A"
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  images: {
+    domains: ['http://127.0.0.1:5002'],
+  },
   reactStrictMode: true,
 }
